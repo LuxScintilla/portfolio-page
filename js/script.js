c1e8b4c0ef76a5a14832366d57f09461d34fbca4
LuxScintilla <105556122+LuxScintilla@users.noreply.github.com>
2023-09-19 12:38:15 +0100

    Fixed mobile nav and horizontal scroll

--- js/script.js
+++ js/script.js
@@ -20,14 +20,31 @@ const obs = new IntersectionObserver(
 );
 obs.observe(sectionHeroEl);
 
-const mobileNavBtn = document.querySelectorAll(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+// const mobileNavBtn = document.querySelectorAll(".btn-mobile-nav");
+// const headerEl = document.querySelector(".header");
 
-mobileNavBtn.forEach(
-  addEventListener("click", function () {
-    headerEl.classList.toggle("nav-open");
-  })
-);
+// mobileNavBtn.forEach(
+//   addEventListener("click", function () {
+//     headerEl.classList.toggle("nav-open");
+//   })
+// );
+
+const btnNavOpen = document.querySelector(".btn-nav-open");
+const btnNavClose = document.querySelector(".btn-nav-close");
+const headerEl = document.querySelector(".header");
+const mainNavLink = document.querySelectorAll(".main-nav-link");
+
+btnNavOpen.addEventListener("click", function () {
+  headerEl.classList.add("nav-open");
+});
+btnNavClose.addEventListener("click", function () {
+  headerEl.classList.remove("nav-open");
+});
+mainNavLink.forEach((link) => {
+  link.addEventListener("click", function () {
+    headerEl.classList.remove("nav-open");
+  });
+});
 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
